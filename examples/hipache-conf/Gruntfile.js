ae50b8aacd9d5f1d97b0d8a97bc2c89e7db6eb0a
Jonas Colmsjö <jonas.colmsjo@gizur.com>
2013-07-18 19:03:31 +0200

    Fixed upgrades grunt

--- examples/hipache-conf/Gruntfile.js
+++ examples/hipache-conf/Gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    pkg: '<json:package.json>',
+    pkg: grunt.file.readJSON('package.json'),
     test: {
       files: ['test/helpers_test.js']
     },
@@ -14,6 +14,7 @@ module.exports = function(grunt) {
       tasks: 'default'
     },
     jshint: {
+      files: ['grunt.js', './*.js', 'test/**/*.js'],
       options: {
         curly: true,
         eqeqeq: true,
@@ -30,10 +31,23 @@ module.exports = function(grunt) {
       globals: {
         exports: true
       }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
     }
   });
 
-  // Default task.
-  grunt.registerTask('default', 'lint test');
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
 
 };
