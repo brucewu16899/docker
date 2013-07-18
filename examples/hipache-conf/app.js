7a049a5038617c948826ad0aa474ef0405cd749c
Jonas Colmsjö <jonas.colmsjo@gizur.com>
2013-07-18 17:59:19 +0200

    Added some redis commands

--- examples/hipache-conf/app.js
+++ examples/hipache-conf/app.js
@@ -9,6 +9,8 @@ client.on("connect", function () {
     client.set("foo_rand000000000000", "some fantastic value", redis.print);
     client.get("foo_rand000000000000", redis.print);
 
+    client.del("frontend:www.dotcloud.com", redis.print);
+
     client.rpush("frontend:www.dotcloud.com", "mywebsite", redis.print);
     client.rpush("frontend:www.dotcloud.com", "http://192.168.0.42:8080", redis.print);
     client.lrange("frontend:www.dotcloud.com", 0,-1, redis.print);
