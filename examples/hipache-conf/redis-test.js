6b60e2757fb5e7f1f657e70f80e901639b068946
Jonas Colmsjö <jonas.colmsjo@gizur.com>
2013-08-02 13:28:06 +0200

    Fix

--- examples/hipache-conf/redis-test.js
+++ examples/hipache-conf/redis-test.js
@@ -13,10 +13,13 @@ redis_client.on("connect", function () {
 
     redis_client.rpush("frontend:www.dotcloud.com", "mywebsite", redis.print);
     redis_client.rpush("frontend:www.dotcloud.com", "http://192.168.0.42:8080", redis.print);
+
     redis_client.lrange("frontend:www.dotcloud.com", 0,-1, redis.print);
+    redis_client.lrange("frontend:www.dotcloud.com", 0,-1, function(res) {
+	console.log('lrange res: '+res);
+    });
 
     redis_client.keys("*", redis.print);
-
     redis_client.keys("*", function(keys) {
 
             console.log('_proxyStatus: redis keys - ' + keys);
