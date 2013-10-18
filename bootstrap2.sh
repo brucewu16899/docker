8c4c5159110a6c04375dfc5a33dc5dddc5aab80e
Jonas Colmsjö <jonas.colmsjo@gizur.com>
2013-10-18 16:34:50 +0200

    Fix

--- bootstrap2.sh
+++ bootstrap2.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 
 #
+# Varaibles used below
+#
+
+HOME=/home/vagrant
+
+#
+# Clone this repo, sharing folders don't always work
+#
+
+sudo su vagrant -c "cd ~ && git clone https://github.com/colmsjo/docker.git"
+
+
+#
 # This script installs the necessary stuff for the docker host.
 # Images for the containers are built with Dockerfiles (see at the bottom)
 #
@@ -17,8 +30,8 @@ sudo apt-get update
 DEBIAN_FRONTEND=noninteractive sudo apt-get install -y lxc-docker
 
 # Need to run docker with other flags, this file need to be updated once the machine is up
-sudo cp ./etc/init/docker.conf /etc/init
-sudo su vagrant -c "echo alias docker='docker -H=tcp://127.0.0.1:4243' >> /home/vagrant/.profile"
+sudo cp $HOME/docker/etc/init/docker.conf /etc/init
+sudo su vagrant -c "echo alias docker='docker -H=tcp://127.0.0.1:4243' >> $HOME/.profile"
 
 #
 # Install redis, used by hipache and redis-dns
@@ -66,7 +79,7 @@ sudo apt-get install -y coffeescript
 sudo npm install hipache -g --production
 sudo npm install redis-dns -g --production
 
-sudo cp ./usr/lib/node_modules/redis-dns/redis-dns-config.json /usr/lib/node_modules/redis-dns
+sudo cp $HOME/docker/usr/lib/node_modules/redis-dns/redis-dns-config.json /usr/lib/node_modules/redis-dns
 sudo ln -s /usr/lib/node_modules/redis-dns/bin/start.sh start-redis-dns
 
 
@@ -80,7 +93,4 @@ sudo npm install grunt grunt-cli -g
 # Use the local nameserver and then google's
 # NOTE: sometimes usefull when using mobile broadband
 #sudo sh -c 'echo "dns-nameservers localhost 8.8.8.8" >> /etc/network/interfaces'
-
-
-sudo su vagrant -c "cd ~ && git clone https://github.com/colmsjo/docker.git"
 
