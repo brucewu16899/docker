a92ec69ae503b690996fd7df9b6d5d0e41063f6e
Jonas Colmsjö <jonas.colmsjo@gizur.com>
2013-10-28 09:54:29 +0100

    Update

--- bin/start.sh
+++ bin/start.sh
@@ -7,8 +7,11 @@
 # redis-cli rpush frontend:www.gizur.com http://[IP ADDRESS]:8080
 #
 
+[[ -n "$1" ]] || ( echo "Usage: start.sh <docker container id> <web app address> <port>"; exit 0 ; )
+
+
 #CONTAINER_ID=$(docker run -d -dns=$REDIS_DNS $1)
 CONTAINER_ID=$(docker run -d  $1)
-redis-cli rpush frontend:www.gizur.com gizur_com
+redis-cli rpush frontend:$2 $2
 IPADDRESS=$(docker inspect $CONTAINER_ID|grep IPAddress|cut -c 23-33)
-redis-cli rpush frontend:www.gizur.com http://$IPADDRESS
+redis-cli rpush frontend:$2 http://$IPADDRESS:$3
