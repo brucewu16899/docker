bbe6c882f26f42b9dcbc84200d37fd7ca954c006
Jonas Colmsjö <jonas.colmsjo@gizur.com>
2013-10-28 10:09:18 +0100

    Fixed

--- bin/start.sh
+++ bin/start.sh
@@ -18,6 +18,7 @@ fi
 
 #CONTAINER_ID=$(docker run -d -dns=$REDIS_DNS $1)
 CONTAINER_ID=$(docker run -d  $1)
+redis-cli del frontend:$2
 redis-cli rpush frontend:$2 $2
 IPADDRESS=$(docker inspect $CONTAINER_ID|grep IPAddress|cut -c 23-33)
 redis-cli rpush frontend:$2 http://$IPADDRESS:$3
