4feb0e5526464a85695e939c7784faa23c68627e
Jonas Colmsjö <jonas.colmsjo@gizur.com>
2013-10-31 08:53:29 +0100

    Added

--- clone-repos.sh
+++ clone-repos.sh
@@ -7,3 +7,4 @@
 sudo su vagrant -c "cd ~ && git clone https://github.com/gizur/gizur-wp-20.git"
 sudo su vagrant -c "cd ~ && git clone https://github.com/colmsjo/openerp-env.git"
 sudo su vagrant -c "cd ~ && git clone https://github.com/colmsjo/jekyll_sandbox.git"
+sudo su vagrant -c "cd ~ && git clone https://github.com/colmsjo/node-docker.git"
