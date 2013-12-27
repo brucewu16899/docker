4d0063380e94d04ce43999682bcd0e95fb38a934
Jonas Colmsjö <jonas.colmsjo@gizur.com>
2013-12-27 19:05:45 +0100

    Added

---
+++ bootstrap-bitcoin.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# See https://launchpad.net/~bitcoin/+archive/bitcoin
+#
+
+
+# For Precise 12.04
+echo "deb http://ppa.launchpad.net/bitcoin/bitcoin/ubuntu precise main" >> /etc/apt/sources.list 
+echo "deb-src http://ppa.launchpad.net/bitcoin/bitcoin/ubuntu precise main" >> /etc/apt/sources.list
+
