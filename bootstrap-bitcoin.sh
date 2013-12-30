6528774bbb331f86b0001cabe2c800bba8779d9f
Jonas Colmsjö <jonas.colmsjo@gizur.com>
2013-12-30 09:41:24 +0100

    Fix

--- bootstrap-bitcoin.sh
+++ bootstrap-bitcoin.sh
@@ -8,7 +8,11 @@
 #sudo sh -c 'echo "deb http://ppa.launchpad.net/bitcoin/bitcoin/ubuntu precise main" >> /etc/apt/sources.list'
 #sudo sh -c 'echo "deb-src http://ppa.launchpad.net/bitcoin/bitcoin/ubuntu precise main" >> /etc/apt/sources.list'
 
-
+# Bitcoin need correct time
+sudo apt-get install -y ntpdate
+sudo ntpdate 0.us.pool.ntp.org
 
 sudo add-apt-repository ppa:bitcoin/bitcoin
 sudo apt-get update
+sudo apt-get install -y bitcoind
+
